88e6406a8b263f7adfdace36e40eb9062a45bb22
Pavel Lymar <60192758+priamoryki@users.noreply.github.com>
2023-03-08 04:09:29 +0300

    add HW-2 solution (#3)

--- lesson2/homework/tagcloud/specification.go
+++ lesson2/homework/tagcloud/specification.go
@@ -2,7 +2,8 @@ package tagcloud
 
 // TagCloud aggregates statistics about used tags
 type TagCloud struct {
-	// TODO: add fields if necessary
+	tags            map[string]*TagStat
+	tagsPrioritized []TagStat
 }
 
 // TagStat represents statistics regarding single tag
@@ -12,17 +13,58 @@ type TagStat struct {
 }
 
 // New should create a valid TagCloud instance
-// TODO: You decide whether this function should return a pointer or a value
-func New() TagCloud {
-	// TODO: Implement this
-	return TagCloud{}
+func New() *TagCloud {
+	return &TagCloud{tags: make(map[string]*TagStat), tagsPrioritized: make([]TagStat, 0)}
 }
 
 // AddTag should add a tag to the cloud if it wasn't present and increase tag occurrence count
 // thread-safety is not needed
-// TODO: You decide whether receiver should be a pointer or a value
-func (TagCloud) AddTag(tag string) {
-	// TODO: Implement this
+func (tagCloud *TagCloud) AddTag(tag string) {
+	_, ok := tagCloud.tags[tag]
+	if !ok {
+		tagCloud.tags[tag] = &TagStat{Tag: tag, OccurrenceCount: 0}
+	}
+	tagCloud.tags[tag].OccurrenceCount += 1
+
+	tagCloud.tagsPrioritized = delPrev(tagCloud.tagsPrioritized, *tagCloud.tags[tag])
+	tagCloud.tagsPrioritized = add(tagCloud.tagsPrioritized, *tagCloud.tags[tag])
+}
+
+func binSearch(a []TagStat, x int) int {
+	l, r := -1, len(a)
+	for r-l != 1 {
+		m := (l + r) / 2
+		if a[m].OccurrenceCount >= x {
+			l = m
+		} else {
+			r = m
+		}
+	}
+	return r
+}
+
+func delPrev(a []TagStat, tagStat TagStat) []TagStat {
+	prevOccurrenceCount := tagStat.OccurrenceCount - 1
+	for i := binSearch(a, prevOccurrenceCount) - 1; i >= 0 && a[i].OccurrenceCount == prevOccurrenceCount; i-- {
+		if a[i].Tag != tagStat.Tag {
+			continue
+		}
+		for j := i; j < len(a)-1; j++ {
+			a[j] = a[j+1]
+		}
+		return a[:len(a)-1]
+	}
+	return a
+}
+
+func add(a []TagStat, tagStat TagStat) []TagStat {
+	id := binSearch(a, tagStat.OccurrenceCount)
+	a = append(a, tagStat)
+	for i := len(a) - 1; i >= id && i > 0; i-- {
+		a[i] = a[i-1]
+	}
+	a[id] = tagStat
+	return a
 }
 
 // TopN should return top N most frequent tags ordered in descending order by occurrence count
@@ -30,8 +72,13 @@ func (TagCloud) AddTag(tag string) {
 // if n is greater that TagCloud size then all elements should be returned
 // thread-safety is not needed
 // there are no restrictions on time complexity
-// TODO: You decide whether receiver should be a pointer or a value
-func (TagCloud) TopN(n int) []TagStat {
-	// TODO: Implement this
-	return nil
+func (tagCloud *TagCloud) TopN(n int) []TagStat {
+	return tagCloud.tagsPrioritized[0:min(len(tagCloud.tags), n)]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
