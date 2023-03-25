f2ad44e160d7247adec0ababd241e2d44e4f68d1
priamoryki <60192758+priamoryki@users.noreply.github.com>
2023-03-25 21:23:26 +0300

    add HW-5 fixes

--- lesson5/homework/pipeline.go
+++ lesson5/homework/pipeline.go
@@ -11,24 +11,28 @@ type (
 
 type Stage func(in In) (out Out)
 
+func chanToChanWithTimeout(ctx context.Context, in In, out chan any) {
+	defer close(out)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case value, ok := <-in:
+			if !ok {
+				return
+			}
+			out <- value
+		}
+	}
+}
+
 func ExecutePipeline(ctx context.Context, in In, stages ...Stage) Out {
 	for _, stage := range stages {
 		inWithTimeout := make(chan any)
-		go func(in In) {
-			defer close(inWithTimeout)
-			for {
-				select {
-				case <-ctx.Done():
-					return
-				case value, ok := <-in:
-					if !ok {
-						return
-					}
-					inWithTimeout <- value
-				}
-			}
-		}(in)
+		go chanToChanWithTimeout(ctx, in, inWithTimeout)
 		in = stage(inWithTimeout)
 	}
-	return in
+	out := make(chan any)
+	go chanToChanWithTimeout(ctx, in, out)
+	return out
 }
