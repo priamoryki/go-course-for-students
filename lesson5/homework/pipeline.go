18103ca46defce6cb4deb6aaaa4c0434c618ba73
Pavel Lymar <60192758+priamoryki@users.noreply.github.com>
2023-03-26 20:12:53 +0300

    HW-5 solution (#6)
    * add HW-5 solution
    
    * fix HW-5 solution
    
    * add HW-5 fixes

--- lesson5/homework/pipeline.go
+++ lesson5/homework/pipeline.go
@@ -11,7 +11,28 @@ type (
 
 type Stage func(in In) (out Out)
 
+func chanToChanWithTimeout(ctx context.Context, in In, out chan any) {
+	defer close(out)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case value, ok := <-in:
+			if !ok {
+				return
+			}
+			out <- value
+		}
+	}
+}
+
 func ExecutePipeline(ctx context.Context, in In, stages ...Stage) Out {
-	// TODO
-	return nil
+	for _, stage := range stages {
+		inWithTimeout := make(chan any)
+		go chanToChanWithTimeout(ctx, in, inWithTimeout)
+		in = stage(inWithTimeout)
+	}
+	out := make(chan any)
+	go chanToChanWithTimeout(ctx, in, out)
+	return out
 }
