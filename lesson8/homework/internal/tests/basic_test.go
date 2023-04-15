40bc5e8d091380f5fb9d7c6f518cbb5a99a4d021
priamoryki <60192758+priamoryki@users.noreply.github.com>
2023-04-15 16:25:58 +0300

    HW-8: add new basic tests

--- lesson8/homework/internal/tests/basic_test.go
+++ lesson8/homework/internal/tests/basic_test.go
@@ -6,6 +6,67 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestCreateUser(t *testing.T) {
+	client := getTestClient()
+
+	userResponse, err := client.createUser("test", "user")
+	assert.NoError(t, err)
+	assert.Zero(t, userResponse.Data.ID)
+	assert.Equal(t, userResponse.Data.Nickname, "test")
+	assert.Equal(t, userResponse.Data.Email, "user")
+}
+
+func TestUpdateUser(t *testing.T) {
+	client := getTestClient()
+
+	_, err := client.createUser("test", "user")
+	assert.NoError(t, err)
+
+	userResponse, err := client.updateUser(0, "test", "user")
+	assert.NoError(t, err)
+	assert.Zero(t, userResponse.Data.ID)
+	assert.Equal(t, userResponse.Data.Nickname, "test")
+	assert.Equal(t, userResponse.Data.Email, "user")
+}
+
+func TestFindUser(t *testing.T) {
+	client := getTestClient()
+
+	_, err := client.createUser("test", "user")
+	assert.NoError(t, err)
+
+	userResponse, err := client.findUser("test")
+	assert.NoError(t, err)
+	assert.Zero(t, userResponse.Data.ID)
+	assert.Equal(t, userResponse.Data.Nickname, "test")
+	assert.Equal(t, userResponse.Data.Email, "user")
+}
+
+func TestListAds(t *testing.T) {
+	client := getTestClient()
+
+	_, err := client.createUser("test", "user")
+	assert.NoError(t, err)
+
+	response, err := client.createAd(0, "hello", "world")
+	assert.NoError(t, err)
+
+	publishedAd, err := client.changeAdStatus(0, response.Data.ID, true)
+	assert.NoError(t, err)
+
+	_, err = client.createAd(0, "best cat", "not for sale")
+	assert.NoError(t, err)
+
+	ads, err := client.listAds(0)
+	assert.NoError(t, err)
+	assert.Len(t, ads.Data, 1)
+	assert.Equal(t, ads.Data[0].ID, publishedAd.Data.ID)
+	assert.Equal(t, ads.Data[0].Title, publishedAd.Data.Title)
+	assert.Equal(t, ads.Data[0].Text, publishedAd.Data.Text)
+	assert.Equal(t, ads.Data[0].AuthorID, publishedAd.Data.AuthorID)
+	assert.True(t, ads.Data[0].Published)
+}
+
 func TestCreateAd(t *testing.T) {
 	client := getTestClient()
 
@@ -43,6 +104,21 @@ func TestChangeAdStatus(t *testing.T) {
 	assert.False(t, response.Data.Published)
 }
 
+func TestGetAd(t *testing.T) {
+	client := getTestClient()
+
+	_, err := client.createUser("test", "user")
+	assert.NoError(t, err)
+
+	response, err := client.createAd(0, "hello", "world")
+	assert.NoError(t, err)
+
+	response, err = client.getAd(0)
+	assert.NoError(t, err)
+	assert.Equal(t, response.Data.Title, "hello")
+	assert.Equal(t, response.Data.Text, "world")
+}
+
 func TestUpdateAd(t *testing.T) {
 	client := getTestClient()
 
@@ -58,7 +134,7 @@ func TestUpdateAd(t *testing.T) {
 	assert.Equal(t, response.Data.Text, "мир")
 }
 
-func TestListAds(t *testing.T) {
+func TestFindAd(t *testing.T) {
 	client := getTestClient()
 
 	_, err := client.createUser("test", "user")
@@ -67,18 +143,8 @@ func TestListAds(t *testing.T) {
 	response, err := client.createAd(0, "hello", "world")
 	assert.NoError(t, err)
 
-	publishedAd, err := client.changeAdStatus(0, response.Data.ID, true)
+	response, err = client.findAd("hello")
 	assert.NoError(t, err)
-
-	_, err = client.createAd(0, "best cat", "not for sale")
-	assert.NoError(t, err)
-
-	ads, err := client.listAds(0)
-	assert.NoError(t, err)
-	assert.Len(t, ads.Data, 1)
-	assert.Equal(t, ads.Data[0].ID, publishedAd.Data.ID)
-	assert.Equal(t, ads.Data[0].Title, publishedAd.Data.Title)
-	assert.Equal(t, ads.Data[0].Text, publishedAd.Data.Text)
-	assert.Equal(t, ads.Data[0].AuthorID, publishedAd.Data.AuthorID)
-	assert.True(t, ads.Data[0].Published)
+	assert.Equal(t, response.Data.Title, "hello")
+	assert.Equal(t, response.Data.Text, "world")
 }
